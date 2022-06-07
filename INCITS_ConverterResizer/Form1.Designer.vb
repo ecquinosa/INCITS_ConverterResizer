7e8636624f84746a5398ca658a180f4fff60d38c
git-allcard <git@allcardtech.com.ph>
2022-06-07 10:29:19 +0800

    initial commit

---
+++ INCITS_ConverterResizer/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(418, 23)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(95, 26)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(317, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 91)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Convert"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(95, 52)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(156, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Output format"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(23, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "File"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 159)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(23, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "File"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(95, 156)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(317, 20)
+        Me.TextBox2.TabIndex = 7
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(418, 153)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Browse"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(18, 185)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(23, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "File"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(95, 182)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(317, 20)
+        Me.TextBox3.TabIndex = 10
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(418, 179)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Browse"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 220)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 12
+        Me.Button5.Text = "Match"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(200, 91)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 13
+        Me.Button6.Text = "Convert"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(512, 134)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "INCITS_Resizer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+
+End Class
---
+++ INCITS_ConverterResizer/Form1.vb
@@ -0,0 +1,180 @@
+﻿
+Imports System.IO
+'Imports SecuGen.FDxSDKPro.Windows
+
+Public Class Form1
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        'If TextBox1.Text = "" Then
+        '    MessageBox.Show("Please enter file..")
+        '    TextBox1.Focus()
+        '    Exit Sub
+        'Else
+        '    If New FileInfo(TextBox1.Text).Length < 1024 Then
+        '        MessageBox.Show("File is not greater than 1024 bytes..")
+        '        TextBox1.Focus()
+        '        Exit Sub
+        '    End If
+        'End If
+
+        Button1.Enabled = False
+        Button2.Enabled = False
+
+        Dim ir As New INCITS_Resizer
+        If ComboBox1.Text = ir.ANSI_378 Then
+            ir.FileType = ir.ANSI_378
+            If ir.Convert(TextBox1.Text, ir.ANSI_378_FileFormat, ir.ISO_19794_2_CF_CS_FileFormat, ir.ISO_19794_2_CF_CS_FileFormat) Then
+                MessageBox.Show("Success!")
+            Else
+                MessageBox.Show("Failed!" & vbNewLine & vbNewLine & ir.ErrorMessage)
+            End If
+        ElseIf ComboBox1.Text = ir.ISO_19794_2_CF_CS Then
+            If ir.ResizeIncits(TextBox1.Text) Then
+                MessageBox.Show("Success!")
+            Else
+                MessageBox.Show("Failed!" & vbNewLine & vbNewLine & ir.ErrorMessage)
+            End If
+        ElseIf ComboBox1.Text = ir.ISO_19794_2 Then
+            If ir.ResizeIncits3(TextBox1.Text) Then
+                MessageBox.Show("Success!")
+            Else
+                MessageBox.Show("Failed!" & vbNewLine & vbNewLine & ir.ErrorMessage)
+            End If
+        ElseIf ComboBox1.Text = ir.Morpho_CFV Then
+            If ir.ResizeIncits(TextBox1.Text) Then
+                MessageBox.Show("Success!")
+            Else
+                MessageBox.Show("Failed!" & vbNewLine & vbNewLine & ir.ErrorMessage)
+            End If
+        End If
+
+        Button1.Enabled = True
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        BrowseFile(TextBox1)
+    End Sub
+
+    Private Sub BrowseFile(ByVal txtbox As TextBox)
+        Dim ofd As New OpenFileDialog
+        If ofd.ShowDialog = Windows.Forms.DialogResult.OK Then
+            txtbox.Text = ofd.FileName
+        End If
+        ofd.Dispose()
+    End Sub
+
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        ComboBox1.Items.Add("ANSI_378")
+        ComboBox1.Items.Add("ISO_19794_2_CF_CS")
+        ComboBox1.Items.Add("ISO_19794_2")
+        ComboBox1.Items.Add("Morpho_CFV")
+        ComboBox1.SelectedIndex = 1
+    End Sub
+
+    'Private m_FPM As New SGFingerPrintManager
+    'Private m_ImageWidth As Int32
+    'Private m_ImageHeight As Int32
+
+    'Private Function SecugenMatchFileTemplate(ByVal ansiFile1 As Byte(), ByVal ansiFile2 As Byte(), ByVal ErrorMessage As String) As Boolean
+    '    Try
+    '        Dim iError As Int32
+    '        Dim matched As Boolean
+
+    '        iError = m_FPM.InitEx(m_ImageWidth, m_ImageHeight, 500)
+    '        iError = m_FPM.MatchAnsiTemplate(ansiFile1, 0, ansiFile2, 0, SGFPMSecurityLevel.NORMAL, matched)
+    '        Application.DoEvents()
+
+    '        If MatchedFingerprints(iError, matched) Then
+    '            Return True
+    '        Else
+    '            Return False
+    '        End If
+    '    Catch ex As Exception
+    '        ErrorMessage = ex.Message
+    '        Return False
+    '    End Try
+
+    'End Function
+
+    'Private Function MatchedFingerprints(ByVal iError As Integer, ByVal matched As Boolean) As Boolean
+    '    If iError = SGFPMError.ERROR_NONE Then
+    '        If matched Then
+    '            Return True
+    '        Else
+    '            Return False
+    '        End If
+    '    Else
+    '        Return False
+    '    End If
+    'End Function
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        BrowseFile(TextBox2)
+    End Sub
+
+    Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
+        BrowseFile(TextBox3)
+    End Sub
+
+    Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
+        'If SecugenMatchFileTemplate(File.ReadAllBytes(TextBox2.Text), File.ReadAllBytes(TextBox3.Text), "") Then
+        '    MessageBox.Show("Matched!")
+        'Else
+        '    MessageBox.Show("Not matched!")
+        'End If
+    End Sub
+
+
+    Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Button6.Click
+        If TextBox1.Text = "" Then
+            MessageBox.Show("Please enter file..")
+            TextBox1.Focus()
+            Exit Sub
+            'Else
+            '    If New FileInfo(TextBox1.Text).Length < 1024 Then
+            '        MessageBox.Show("File is not greater than 1024 bytes..")
+            '        TextBox1.Focus()
+            '        Exit Sub
+            '    End If
+        End If
+
+        Button1.Enabled = False
+        Button2.Enabled = False
+
+        Dim ir As New INCITS_Resizer
+        ir.FileType = ir.Morpho_CFV
+        If ir.Convert(TextBox1.Text, ir.ANSI_378_FileFormat, ir.Morpho_CFV_FileFormat, ir.Morpho_CFV_EXT) Then
+            MessageBox.Show("Success!")
+        Else
+            MessageBox.Show("Failed!" & vbNewLine & vbNewLine & ir.ErrorMessage)
+        End If
+
+        Return
+
+        If ComboBox1.Text = ir.ANSI_378 Then
+            ir.FileType = ir.Morpho_CFV
+            If ir.Convert(TextBox1.Text, ir.ANSI_378_FileFormat, ir.Morpho_CFV_FileFormat, ir.Morpho_CFV_FileFormat) Then
+                MessageBox.Show("Success!")
+            Else
+                MessageBox.Show("Failed!" & vbNewLine & vbNewLine & ir.ErrorMessage)
+            End If
+        ElseIf ComboBox1.Text = ir.ISO_19794_2_CF_CS Then
+            If ir.ResizeIncits(TextBox1.Text) Then
+                MessageBox.Show("Success!")
+            Else
+                MessageBox.Show("Failed!" & vbNewLine & vbNewLine & ir.ErrorMessage)
+            End If
+        ElseIf ComboBox1.Text = ir.Morpho_CFV Then
+            If ir.ResizeIncits(TextBox1.Text) Then
+                MessageBox.Show("Success!")
+            Else
+                MessageBox.Show("Failed!" & vbNewLine & vbNewLine & ir.ErrorMessage)
+            End If
+        End If
+
+        Button1.Enabled = True
+        Button2.Enabled = True
+    End Sub
+End Class
